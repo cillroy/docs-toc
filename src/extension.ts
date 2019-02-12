f1bd82529755941cb122b84cd598593d05053ec6
Cillroy <curt@cullens.com>
2019-02-11 16:59:28 -0800

    update let to const

--- src/extension.ts
+++ src/extension.ts
@@ -21,8 +21,10 @@ export function activate(context: vscode.ExtensionContext) {
 				// Launch dmc-toc-helper in browser with yaml or md querystring
 				// https://code.visualstudio.com/api/references/vscode-api#env
 				// IGNORE ERROR AND LAUNCH DEBUG
-				let querystring = vscode.window.activeTextEditor.document.getText();
-				let lang = vscode.window.activeTextEditor.document.languageId;
+
+				const editor: vscode.TextEditor = vscode.window.activeTextEditor;
+				const querystring = editor.document.getText();
+				const lang = editor.document.languageId;
 				vscode.env.openExternal(vscode.Uri.parse('https://cillroy.github.io/dmc-toc-helper/?' + (lang === 'markdown' ? 'markdown' : 'yaml') + '=' + encodeURIComponent(escape(querystring))));
 			} catch {
 				vscode.window.showErrorMessage('There was a problem reading your table of contents file, please make sure you have your file open and it is the active window.');
