efc3c7df33c880f08565c08119e62217f6f37b24
Cillroy <curt@cullens.com>
2019-02-12 10:47:17 -0800

    clearing errors to compile

--- out/extension.js
+++ out/extension.js
@@ -4,16 +4,16 @@ const vscode = require("vscode");
 function activate(context) {
     const dmcURI = 'https://cillroy.github.io/dmc-toc-helper/?';
     context.subscriptions.push(vscode.commands.registerCommand('extension.docs-toc', () => {
-        try {
-            // Launch dmc-toc-helper in browser with yaml or md querystring
-            // https://code.visualstudio.com/api/references/vscode-api#env
-            // IGNORE ERROR AND LAUNCH DEBUG
+        // Launch dmc-toc-helper in browser with yaml or md querystring
+        // https://code.visualstudio.com/api/references/vscode-api#env
+        // IGNORE ERROR AND LAUNCH DEBUG
+        if (vscode.window.activeTextEditor !== undefined) {
             const editor = vscode.window.activeTextEditor;
             const lang = editor.document.languageId;
             const querystring = editor.document.getText();
             vscode.env.openExternal(vscode.Uri.parse(dmcURI + (lang === 'markdown' ? 'markdown' : 'yaml') + '=' + encodeURIComponent(escape(querystring))));
         }
-        catch (_a) {
+        else {
             vscode.window.showErrorMessage('There was a problem reading your table of contents file, please make sure you have your file open and it is the active window.');
         }
     }));
--- src/extension.ts
+++ src/extension.ts
@@ -5,16 +5,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('extension.docs-toc', () => {
-			try {
-				// Launch dmc-toc-helper in browser with yaml or md querystring
-				// https://code.visualstudio.com/api/references/vscode-api#env
-				// IGNORE ERROR AND LAUNCH DEBUG
-
+			// Launch dmc-toc-helper in browser with yaml or md querystring
+			// https://code.visualstudio.com/api/references/vscode-api#env
+			// IGNORE ERROR AND LAUNCH DEBUG
+			if (vscode.window.activeTextEditor !== undefined) {
 				const editor: vscode.TextEditor = vscode.window.activeTextEditor;
 				const lang = editor.document.languageId;
 				const querystring = editor.document.getText();
 				vscode.env.openExternal(vscode.Uri.parse(dmcURI + (lang === 'markdown' ? 'markdown' : 'yaml') + '=' + encodeURIComponent(escape(querystring))));
-			} catch {
+			} else {
 				vscode.window.showErrorMessage('There was a problem reading your table of contents file, please make sure you have your file open and it is the active window.');
 			}
 		}));
