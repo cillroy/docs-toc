fb8f079686eee29113799a58b8d1662f804a3da5
Cillroy <curt@cullens.com>
2019-02-11 17:19:31 -0800

    refactor for launch

--- out/extension.js
+++ out/extension.js
@@ -1,23 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const path = require("path");
 const vscode = require("vscode");
 function activate(context) {
-    console.log('Congratulations, your extension "docs-toc" is now active!');
-    const dmcPath = path.join(context.extensionPath, 'dmc-toc-helper');
+    const dmcURI = 'https://cillroy.github.io/dmc-toc-helper/?';
     context.subscriptions.push(vscode.commands.registerCommand('extension.docs-toc', () => {
-        let displayDate = new Date().toLocaleDateString();
-        vscode.window.showInformationMessage('Docs: Table of Content Helper (' + displayDate + ')');
-    }));
-    console.log('Launching HTML based tool');
-    context.subscriptions.push(vscode.commands.registerCommand('extension.docs-toc-html', () => {
         try {
             // Launch dmc-toc-helper in browser with yaml or md querystring
             // https://code.visualstudio.com/api/references/vscode-api#env
             // IGNORE ERROR AND LAUNCH DEBUG
-            let querystring = vscode.window.activeTextEditor.document.getText();
-            let lang = vscode.window.activeTextEditor.document.languageId;
-            vscode.env.openExternal(vscode.Uri.parse('https://cillroy.github.io/dmc-toc-helper/?' + (lang === 'markdown' ? 'markdown' : 'yaml') + '=' + encodeURIComponent(escape(querystring))));
+            const editor = vscode.window.activeTextEditor;
+            const lang = editor.document.languageId;
+            const querystring = editor.document.getText();
+            vscode.env.openExternal(vscode.Uri.parse(dmcURI + (lang === 'markdown' ? 'markdown' : 'yaml') + '=' + encodeURIComponent(escape(querystring))));
         }
         catch (_a) {
             vscode.window.showErrorMessage('There was a problem reading your table of contents file, please make sure you have your file open and it is the active window.');
--- src/extension.ts
+++ src/extension.ts
@@ -2,30 +2,19 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "docs-toc" is now active!');
-	const dmcPath: string = path.join(context.extensionPath, 'dmc-toc-helper');
+	const dmcURI: string = 'https://cillroy.github.io/dmc-toc-helper/?';
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('extension.docs-toc', () => {
-			let displayDate = new Date().toLocaleDateString();
-
-			vscode.window.showInformationMessage('Docs: Table of Content Helper (' + displayDate + ')');
-		}));
-
-	console.log('Launching HTML based tool');
-
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('extension.docs-toc-html', () => {
 			try {
 				// Launch dmc-toc-helper in browser with yaml or md querystring
 				// https://code.visualstudio.com/api/references/vscode-api#env
 				// IGNORE ERROR AND LAUNCH DEBUG
 
 				const editor: vscode.TextEditor = vscode.window.activeTextEditor;
-				const querystring = editor.document.getText();
 				const lang = editor.document.languageId;
-				vscode.env.openExternal(vscode.Uri.parse('https://cillroy.github.io/dmc-toc-helper/?' + (lang === 'markdown' ? 'markdown' : 'yaml') + '=' + encodeURIComponent(escape(querystring))));
+				const querystring = editor.document.getText();
+				vscode.env.openExternal(vscode.Uri.parse(dmcURI + (lang === 'markdown' ? 'markdown' : 'yaml') + '=' + encodeURIComponent(escape(querystring))));
 			} catch {
 				vscode.window.showErrorMessage('There was a problem reading your table of contents file, please make sure you have your file open and it is the active window.');
 			}
