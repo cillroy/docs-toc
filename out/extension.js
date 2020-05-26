606fd9ab72dd1826301b689c00768cb5ac14cc74
cillroy <curtcu@microsoft.com>
2020-05-26 12:30:47 -0500

    update for local to view html and online

--- out/extension.js
+++ out/extension.js
@@ -3,17 +3,42 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
 function activate(context) {
     const dmcURI = 'https://cillroy.github.io/dmc-toc-helper/?';
-    context.subscriptions.push(vscode.commands.registerCommand('extension.docs-toc', () => {
+    const activeEditorError = 'There was a problem reading your table of contents file, please make sure you have your file open and it is the active window.';
+    context.subscriptions.push(vscode.commands.registerCommand('extension.docs-toc-local', () => {
         if (vscode.window.activeTextEditor !== undefined) {
             const editor = vscode.window.activeTextEditor;
             const lang = editor.document.languageId;
             const querystring = editor.document.getText();
-            vscode.env.openExternal(vscode.Uri.parse(dmcURI + (lang === 'markdown' ? 'markdown' : 'yaml') + '=' + encodeURIComponent(escape(querystring))));
+            sendToWebPanel(querystring);
         }
         else {
-            vscode.window.showErrorMessage('There was a problem reading your table of contents file, please make sure you have your file open and it is the active window.');
+            vscode.window.showErrorMessage(activeEditorError);
+        }
+    }), vscode.commands.registerCommand('extension.docs-toc-online', () => {
+        if (vscode.window.activeTextEditor !== undefined) {
+            const editor = vscode.window.activeTextEditor;
+            const lang = editor.document.languageId;
+            const querystring = editor.document.getText();
+            sendToHosted(lang, querystring);
+        }
+        else {
+            vscode.window.showErrorMessage(activeEditorError);
         }
     }));
+    function sendToWebPanel(querystring) {
+        // Create and show panel
+        const panel = vscode.window.createWebviewPanel('dmc-toc-helper', 'ToC Helper', vscode.ViewColumn.One, {});
+        // And set its HTML content
+        panel.webview.html = convertToC(querystring);
+    }
+    function sendToHosted(lang, querystring) {
+        vscode.env.openExternal(vscode.Uri.parse(dmcURI + (lang === 'markdown' ? 'markdown' : 'yaml') + '=' + encodeURIComponent(escape(querystring))));
+    }
+    function convertToC(querystring) {
+        var oHtml = "";
+        oHtml = querystring;
+        return oHtml;
+    }
 }
 exports.activate = activate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -2,16 +2,54 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	const dmcURI: string = 'https://cillroy.github.io/dmc-toc-helper/?';
+	const activeEditorError: string = 'There was a problem reading your table of contents file, please make sure you have your file open and it is the active window.';
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand('extension.docs-toc', () => {
+		vscode.commands.registerCommand('extension.docs-toc-local', () => {
+
 			if (vscode.window.activeTextEditor !== undefined) {
 				const editor: vscode.TextEditor = vscode.window.activeTextEditor;
 				const lang = editor.document.languageId;
 				const querystring = editor.document.getText();
-				vscode.env.openExternal(vscode.Uri.parse(dmcURI + (lang === 'markdown' ? 'markdown' : 'yaml') + '=' + encodeURIComponent(escape(querystring))));
-			} else {
-				vscode.window.showErrorMessage('There was a problem reading your table of contents file, please make sure you have your file open and it is the active window.');
+				sendToWebPanel(querystring);
 			}
-		}));
+			else {
+				vscode.window.showErrorMessage(activeEditorError);
+			}
+		}),
+		vscode.commands.registerCommand('extension.docs-toc-online', () => {
+
+			if (vscode.window.activeTextEditor !== undefined) {
+				const editor: vscode.TextEditor = vscode.window.activeTextEditor;
+				const lang = editor.document.languageId;
+				const querystring = editor.document.getText();
+				sendToHosted(lang, querystring);
+			}
+			else {
+				vscode.window.showErrorMessage(activeEditorError);
+			}
+		})
+	);
+
+	function sendToWebPanel(querystring: string) {
+		// Create and show panel
+		const panel = vscode.window.createWebviewPanel(
+			'dmc-toc-helper',
+			'ToC Helper',
+			vscode.ViewColumn.One,
+			{}
+		);
+
+		// And set its HTML content
+		panel.webview.html = convertToC(querystring);
+	}
+	function sendToHosted(lang: string, querystring: string) {
+		vscode.env.openExternal(vscode.Uri.parse(dmcURI + (lang === 'markdown' ? 'markdown' : 'yaml') + '=' + encodeURIComponent(escape(querystring))));
+	}
+
+	function convertToC(querystring: string): string {
+		var oHtml = "";
+		oHtml = querystring;
+		return oHtml;
+	}
 }
